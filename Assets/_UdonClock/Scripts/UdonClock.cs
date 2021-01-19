f71d937b6f4d39a9d84a9c6badb61e1a344c5d23
Dustuu <dustuuvr@gmail.com>
2021-01-19 04:16:20 -0500

    added unity project

---
+++ Assets/_UdonClock/Scripts/UdonClock.cs
@@ -0,0 +1,31 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using UnityEngine.UI;
+using VRC.SDKBase;
+
+namespace Dustuu.VRChat.UdonClockSystem
+{
+    [RequireComponent(typeof(Text))]
+    public class UdonClock : UdonSharpBehaviour
+    {
+        [SerializeField]
+        private bool useServerTime = false;
+        [SerializeField]
+        private bool useMilitaryTime = false;
+        [SerializeField]
+        private bool hideSeconds = false;
+        private Text text;
+
+        protected void Start() { text = GetComponent<Text>(); }
+
+        protected void Update() { text.text = GetDateTimeString(); }
+
+        private string GetDateTimeString()
+        {
+            string dateTimeFormat = useMilitaryTime ? (hideSeconds ? "{0:HH:mm}" : "{0:HH:mm:ss}") : (hideSeconds ? "{0:hh:mm tt}" : "{0:hh:mm:ss tt}");
+            System.DateTime dateTime = useServerTime ? Networking.GetNetworkDateTime() : System.DateTime.Now;
+            return string.Format(dateTimeFormat, dateTime);
+        }
+    }
+}
